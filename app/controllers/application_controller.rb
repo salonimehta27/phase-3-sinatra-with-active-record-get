9bff2db30031ee27894d7554566311e5343841a8
Saloni <salonimehta2127@gmail.com>
2021-09-29 21:48:13 -0400

    Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < Sinatra::Base
-
+set :default_content_type, 'application/json'
   get '/' do
     { message: "Hello world" }.to_json
+  end
+  get '/games' do
+    Game.all.order(:title).limit(10).to_json
+  end
+  get '/games/:id' do
+  game= Game.find(params[:id])
+  game.to_json(only:[:id,:title,:genre,:price],include: {reviews:
+    {only:[:score,:comment], include: {user: {only: [:name]}
+  }}
+  })
   end
 
 end
